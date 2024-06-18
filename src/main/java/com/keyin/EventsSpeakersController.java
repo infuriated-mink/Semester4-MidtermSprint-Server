ad0ef816a975a4ce7445950f80e2ea460cfd05c2
Evan Harte <126525806+evanharte@users.noreply.github.com>
2024-06-18 13:35:52 -0230

    Added EventsSpeakers.java and corresponding Controller and Service files... Not sure if I did this right, but they look a lot like the other entities so hopefully they are ok.

---
+++ src/main/java/com/keyin/EventsSpeakersController.java
@@ -0,0 +1,48 @@
+package com.keyin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // need this to be able to access say http://localhost:8080/hello
+@CrossOrigin
+public class EventsSpeakersController {
+    @Autowired
+    private EventsSpeakersService eventsSpeakersService;
+
+    @GetMapping("search_events_speakers")
+    public List<EventsSpeakers> searchEventsSpeakersByEventId(@RequestParam(value = "eventId", required = false) Integer eventId) {
+        return eventsSpeakersService.findEventsSpeakersByEventId(eventId);
+    }
+
+    @GetMapping("search_events_speakers")
+    public List<EventsSpeakers> searchEventsSpeakersBySpeakerId(@RequestParam(value = "speakerId", required = false) Integer speakerId) {
+        return eventsSpeakersService.findEventsSpeakersBySpeakerId(speakerId);
+    }
+
+    @GetMapping("events_speakers")
+    public List<EventsSpeakers> getEventsSpeakers() {
+        return eventsSpeakersService.getAllEventsSpeakers();
+    }
+
+    @GetMapping("events_speakers/{id}")
+    public EventsSpeakers getEventsSpeakersById(@PathVariable Integer id) {
+        return eventsSpeakersService.getEventsSpeakers(id);
+    }
+
+    @PostMapping("events_speakers")
+    public EventsSpeakers addEventsSpeakers(@RequestBody EventsSpeakers eventsSpeakers) {
+        return eventsSpeakersService.createEventsSpeakers(eventsSpeakers);
+    }
+
+    @PutMapping("events_speakers/{id}")
+    public EventsSpeakers updateEventsSpeakers(@PathVariable Integer id, @RequestBody EventsSpeakers updatedEventsSpeakers) {
+        return eventsSpeakersService.updateEventsSpeakers(id, updatedEventsSpeakers);
+    }
+
+    @DeleteMapping("events_speakers/{id}")
+    public void deleteEventsSpeakers(@PathVariable Integer id) {
+        eventsSpeakersService.deleteEventsSpeakers(id);
+    }
+}
